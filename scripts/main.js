08b019a373df7f62f84096b993c01365ace9daaf
Jesse Vorisek <jesse.vorisek@gmail.com>
2020-01-13 18:00:10 -0800

    Added support for phases. Also added a title.

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,8 @@
-let speedup = 10;		// development knob
-
 // Returns a number in [min, max).
 function random_int(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;	// note that the seed is chosen by the browser
-}
-
-function lookup(key) {
-	let value = strings[key];
-	if (value) {
-		return value;
-	}
-	console.log("Couldn't find a string for '" + key + "'");
-	return key;
 }
 
 function choose(from) {
@@ -32,34 +21,85 @@ function choose(from) {
 	}
 }
 
-function update_progress(interval, value) {
+function update_progress(context) {
 	var progress = document.querySelector("#progress");
-	if (value < 100) {
-		progress.value = value;
-		setTimeout(update_progress, interval, interval, value+1);
+	if (context.value < 100) {
+		context.value += 1;
+		progress.value = context.value;
+		setTimeout(update_progress, context.interval, context);
 	} else {
 		progress.value = 0;
-		start_task();
+		context.is_action = !context.is_action;
+		if (context.phase == 0) {
+			context.phase = 1;
+			context.start = new Date().getTime();
+		}
+		begin(context);
 	}
 }
 
-function start_task() {
-	let entry = choose(tasks);
-	var task = document.querySelector("#task");
-	task.textContent = entry.title;
+function begin(context) {
+	let elapsed = (new Date().getTime() - context.start)/1000;
+	var duration = settings.duration ? settings.duration : phases[context.phase].secs;	// can't just use settings.speed because that makes the duration way too small
+	if (elapsed >= duration) {
+		context.phase += 1;
+		context.start = new Date().getTime();
+	}
+	
+	if (context.phase < phases.length-2) {
+		let entry = context.is_action ? choose(tasks) : choose(rest);
+		var label = document.querySelector("#label");
+		label.textContent = entry.title;
+	
+		var sublabel = document.querySelector("#sublabel");
+		sublabel.textContent = context.is_action ? phases[context.phase].sublabel : "";
+	
+		duration = entry.secs*1000*phases[context.phase].speed*settings.speed;
+		context.interval = duration/100;
+		context.value = 0;
+		setTimeout(update_progress, context.interval, context);
+	
+	} else if (context.phase == phases.length-1) {
+		var sublabel = document.querySelector("#sublabel");
+		sublabel.textContent = phases[context.phase].sublabel;
 
-	let duration = entry.secs*1000/speedup;
-	let interval = duration/100;
-	setTimeout(update_progress, interval, interval, 0);
+	} else {
+		var label = document.querySelector("#label");
+		label.textContent = "";
+	
+		var x = Math.random();
+		if (x <= settings.prob) {		
+			var sublabel = document.querySelector("#sublabel");
+			sublabel.textContent = phases[context.phase].sublabel;
+
+			duration = phases[context.phase].secs*1000*phases[context.phase].speed*settings.speed;
+			context.interval = duration/100;
+			context.value = 0;
+
+			context.phase += 1;
+			setTimeout(update_progress, context.interval, context);
+			
+		} else {
+			var sublabel = document.querySelector("#sublabel");
+			sublabel.textContent = settings.lost;
+		}
+	}
 }
 
 window.addEventListener("DOMContentLoaded", function(){
 	// selectors: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors
-	var title = document.querySelector("title");
-	title.textContent = lookup("window-title");
+	var wtitle = document.querySelector("title");
+	wtitle.textContent = settings.window_title;
 	
-	setTimeout(start_task, 1);
-	// XXX
-	// alternate between tasks and rest
-	// add speedup to the data file
+	var title = document.querySelector("#title");
+	title.textContent = settings.title;
+	
+	var label = document.querySelector("#label");
+	var sublabel = document.querySelector("#sublabel");
+	label.textContent = phases[0].label;
+	sublabel.textContent = phases[0].sublabel;
+
+	let duration = phases[0].secs*1000*settings.speed;
+	let context = {phase: 0, start: new Date().getTime(), is_action: true, interval: duration/100, value: 0};
+	setTimeout(update_progress, context.interval, context);
 });
